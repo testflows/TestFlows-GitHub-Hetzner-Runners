d54c852370190c434e5632b324f533a184bd004b
vzakaznikov <vzakaznikov@protonmail.com>
2023-07-26 22:26:06 -0400

    Updating default setup.sh to add ubuntu user to the docker group.

--- testflows/github/runners/scripts/setup.sh
+++ testflows/github/runners/scripts/setup.sh
@@ -5,5 +5,7 @@ echo "Create and configure ubuntu user"
 adduser ubuntu --disabled-password --gecos ""
 echo "%wheel   ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
 addgroup wheel
+addgroup docker
 usermod -aG wheel ubuntu
 usermod -aG sudo ubuntu
+usermod -aG docker ubuntu
