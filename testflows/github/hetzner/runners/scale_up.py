e34251f552f196a63da34ad0ffb00c2bad1785ff
Vitaliy Zakaznikov <vzakaznikov@testflows.com>
2024-03-18 12:01:04 -0400

    Fixing runner_name check to handle the case where runner_name is None.

--- testflows/github/hetzner/runners/scale_up.py
+++ testflows/github/hetzner/runners/scale_up.py
@@ -95,7 +95,7 @@ def get_runner_server_type_and_location(runner_name: str):
     """Determine runner's server type, and location."""
     server_type, server_location = None, None
 
-    if runner_name.startswith(runner_name_prefix):
+    if runner_name and runner_name.startswith(runner_name_prefix):
         if len(runner_name.split("-")) == 7:
             server_type, server_location = runner_name.split("-")[5:]
 
