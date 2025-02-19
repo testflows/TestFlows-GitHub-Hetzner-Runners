18e1c5460f396bc7499dc223f0a5597ac331fa0f
Vitaliy Zakaznikov <vzakaznikov@testflows.com>
2025-02-19 17:55:49 -0500

    Updated setup script structure.

--- testflows/github/hetzner/runners/scripts/setup.sh
+++ testflows/github/hetzner/runners/scripts/setup.sh
@@ -1,19 +1,22 @@
 set -x
 
-echo "Create and configure ubuntu user"
+{
+    echo "Create and configure ubuntu user"
+    adduser ubuntu --disabled-password --gecos ""
+    echo "%wheel   ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
+    addgroup wheel
+    addgroup docker
+    usermod -aG wheel ubuntu
+    usermod -aG sudo ubuntu
+    usermod -aG docker ubuntu
+}
 
-adduser ubuntu --disabled-password --gecos ""
-echo "%wheel   ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
-addgroup wheel
-addgroup docker
-usermod -aG wheel ubuntu
-usermod -aG sudo ubuntu
-usermod -aG docker ubuntu
-
-echo "Install fail2ban"
-apt-get update
-apt-get install --yes --no-install-recommends \
-    fail2ban 
+{
+    echo "Install fail2ban"
+    apt-get update
+    apt-get install --yes --no-install-recommends \
+        fail2ban 
     
-echo "Launch fail2ban"
-systemctl start fail2ban
+    echo "Launch fail2ban"
+    systemctl start fail2ban
+}
