0ea4090d966ac146c3647b49abce6aec6f2ea69b
vzakaznikov <41681088+vzakaznikov@users.noreply.github.com>
2025-03-11 15:07:10 -0400

    Update setup.sh to install fail2ban

--- testflows/github/hetzner/runners/scripts/deploy/setup.sh
+++ testflows/github/hetzner/runners/scripts/deploy/setup.sh
@@ -1,22 +1,38 @@
 
 set -x
 
-apt-get update
+{
+    echo "Install required packages"
+    apt-get update
+    apt-get -y install python3-pip
+    apt-get -y install openssh-client
+}
 
-apt-get -y install python3-pip
-apt-get -y install openssh-client
+{
+    echo "Create and configure ubuntu user"
+    adduser ubuntu --disabled-password --gecos ""
+    echo "%wheel   ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
+    addgroup wheel
+    usermod -aG wheel ubuntu
+    usermod -aG sudo ubuntu
+}
 
-echo "Create and configure ubuntu user"
+{
+    echo "Install fail2ban"
+    apt-get update
+    apt-get install --yes --no-install-recommends \
+        fail2ban
+    echo "Launch fail2ban"
+    systemctl start fail2ban
+}
 
-adduser ubuntu --disabled-password --gecos ""
-echo "%wheel   ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
-addgroup wheel
-usermod -aG wheel ubuntu
-usermod -aG sudo ubuntu
+{
+    echo "Generate SSH Key"
+    sudo -u ubuntu ssh-keygen -t rsa -q -f "/home/ubuntu/.ssh/id_rsa" -N ""
+}
 
-echo "Generate SSH Key"
-sudo -u ubuntu ssh-keygen -t rsa -q -f "/home/ubuntu/.ssh/id_rsa" -N ""
-
-echo "Create scripts folder"
-mkdir -p /home/ubuntu/.github-hetzner-runners/scripts
-mkdir -p /home/ubuntu/.github-hetzner-runners/configs
+{
+    echo "Create scripts folder"
+    mkdir -p /home/ubuntu/.github-hetzner-runners/scripts
+    mkdir -p /home/ubuntu/.github-hetzner-runners/configs
+}
