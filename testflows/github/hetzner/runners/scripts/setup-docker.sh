59b9ce28c95482f923e6cd9f625d7d5108561c57
Vitaliy Zakaznikov <vzakaznikov@testflows.com>
2025-05-12 13:19:48 -0400

    Updating docker setup scripts to use the latest instructions from https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository.

--- testflows/github/hetzner/runners/scripts/setup-docker.sh
+++ testflows/github/hetzner/runners/scripts/setup-docker.sh
@@ -27,18 +27,18 @@ set -x
 
     echo "Add Docker’s official GPG key"
     install -m 0755 -d /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    chmod a+r /etc/apt/keyrings/docker.gpg
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    chmod a+r /etc/apt/keyrings/docker.asc
 
     echo "Set up Docker's repository"
     echo \
-    "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
     tee /etc/apt/sources.list.d/docker.list > /dev/null
 
     echo "Install Docker Engine and containerd"
     apt-get -y update
-    apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin
+    apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
     echo "Add ubuntu user to docker group"
     usermod -aG docker ubuntu
