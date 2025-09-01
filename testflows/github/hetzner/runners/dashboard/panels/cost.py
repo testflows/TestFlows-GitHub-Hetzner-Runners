ec48c43328c4e2897ee33ed7463622c38f540375
Vitaliy Zakaznikov <vzakaznikov@testflows.com>
2025-08-31 20:28:33 -0400

    More clean up of panels code.

--- testflows/github/hetzner/runners/dashboard/panels/cost.py
+++ testflows/github/hetzner/runners/dashboard/panels/cost.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import altair as alt
 import streamlit as st
 import pandas as pd
 from datetime import datetime, timedelta
