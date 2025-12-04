e83c97f487fd4bfd1d1e189fab311969beeb5670
Tobias Ludwig Maier <me@tobiasmaier.info>
2025-12-04 23:07:18 +0100

    Upgrade GitHub Actions Runner from 2.306.0 to 2.330.0
    Update both x64 and arm64 startup scripts to use the latest GitHub
    Actions Runner version with corresponding SHA256 checksums.
    
    Changes:
    - Bump runner version from 2.306.0 (July 2023) to 2.330.0 (Nov 2025)
    - Refactor scripts to use ACTIONS_RUNNER_VERSION and ACTIONS_RUNNER_ARCH
      variables for easier future updates
    - Place version and checksum at the top of the file for quick access
    - Add comment with link to releases page for reference
    - Use $ACTIONS_RUNNER_FILE variable in tar command instead of hardcoded
      filename

--- testflows/github/hetzner/runners/scripts/startup-x64.sh
+++ testflows/github/hetzner/runners/scripts/startup-x64.sh
@@ -2,9 +2,14 @@ set -x
 echo "Install runner"
 cd /home/ubuntu
 
-ACTIONS_RUNNER_FILE="actions-runner-linux-x64-2.306.0.tar.gz"
-ACTIONS_RUNNER_SHA256="b0a090336f0d0a439dac7505475a1fb822f61bbb36420c7b3b3fe6b1bdc4dbaa"
-ACTIONS_RUNNER_URL="https://github.com/actions/runner/releases/download/v2.306.0/$ACTIONS_RUNNER_FILE"
+# GitHub Actions Runner - update version and checksum when upgrading
+# https://github.com/actions/runner/releases
+ACTIONS_RUNNER_VERSION="2.330.0"
+ACTIONS_RUNNER_SHA256="af5c33fa94f3cc33b8e97937939136a6b04197e6dadfcfb3b6e33ae1bf41e79a"
+
+ACTIONS_RUNNER_ARCH="x64"
+ACTIONS_RUNNER_FILE="actions-runner-linux-${ACTIONS_RUNNER_ARCH}-${ACTIONS_RUNNER_VERSION}.tar.gz"
+ACTIONS_RUNNER_URL="https://github.com/actions/runner/releases/download/v${ACTIONS_RUNNER_VERSION}/$ACTIONS_RUNNER_FILE"
 CACHE_DIR="${CACHE_DIR:-/mnt/cache}"
 CACHE_DIR_GITHUB="$CACHE_DIR/github"
 
@@ -40,7 +45,7 @@ else
     download_and_verify
 fi
 
-tar xzf ./actions-runner-linux-x64-2.306.0.tar.gz
+tar xzf "./$ACTIONS_RUNNER_FILE"
 
 echo "Configure runner"
 ./config.sh --unattended --replace --url https://github.com/${GITHUB_REPOSITORY} --token ${GITHUB_RUNNER_TOKEN} --name "$(hostname)-${SERVER_TYPE_NAME}-${SERVER_LOCATION_NAME}" --runnergroup "${GITHUB_RUNNER_GROUP}" --labels "${GITHUB_RUNNER_LABELS}" --work _work --ephemeral
